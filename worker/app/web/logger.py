03dd9934f6e591d464a1bd889705bed3b8d1aebe
malivartti <malivartti@yandex.ru>
2025-04-25 05:08:08 +0300

    delete logging to file

--- worker/app/web/logger.py
+++ worker/app/web/logger.py
@@ -1,5 +1,4 @@
 import logging
-import os
 import typing
 
 if typing.TYPE_CHECKING:
@@ -10,25 +9,14 @@ def setup_logging(_: "Application") -> None:
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
-    log_path = os.path.join(
-        os.path.dirname(
-            os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        ),
-        "logs",
-    )
-    file_handler = logging.FileHandler(log_path)
-    file_handler.setLevel(logging.INFO)
-
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
 
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-    file_handler.setFormatter(formatter)
     console_handler.setFormatter(formatter)
 
     logger.handlers.clear()
 
-    logger.addHandler(file_handler)
     logger.addHandler(console_handler)
 
     logging.info("Starting app")
